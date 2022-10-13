7fe2e4f9c3f1ef50ad8f1cfc3a90d981e42ec353
Arwalu13 <108233662+Arwalu13@users.noreply.github.com>
2022-10-12 20:30:44 -0400

    almost complete

--- CCC00J1.py
+++ CCC00J1.py
@@ -1,2 +1,32 @@
-temp = input()
-startDay, numOfDays = temp.split(" ")
+temp = input().split(" ")
+startDay = int(temp[0])
+numOfDays = int(temp[1])
+
+currentDay = startDay
+
+print("Sun Mon Tue Wed Thr Fri Sat")
+
+if startDay == 1:
+    print("", end = "")
+
+else:
+    print(" " * ((startDay -1) * 4), end = "")
+
+for i in range(1, numOfDays + 1):
+    if currentDay == 8:
+        currentDay = 1
+        print("")
+    
+    if i <= 9:
+        print(f"  {i}", end = "")
+    
+    else: 
+        print(f" {i}", end = "")
+
+    currentDay += 1
+
+    
+    if currentDay != 0 and i != numOfDays:
+        print(" ", end = "")
+
+print("")
