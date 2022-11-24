62fc0c0c01c9f642a3f19014a8c5f43153f5ede8
Arwalu13 <108233662+Arwalu13@users.noreply.github.com>
2022-11-23 20:31:37 -0500

    stoooof

--- j2/CCC11J2.py
+++ j2/CCC11J2.py
@@ -1,8 +1,13 @@
 h = int(input())
 m = int(input())
+touchGround = False
 
-for i in range(0, m):
+for i in range(1, m + 1):
     if -6*i**4 + h*i**3 + 2*i**2 + i <= 0:
         print("The balloon first touches ground at hour:")
         print(i)
+        touchGround = True
         break
+
+if touchGround == False:
+    print("The balloon does not touch ground in the given time.")
