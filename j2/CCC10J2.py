4367273f25546c05c743f99d940c2b582add6496
Arwalu13 <108233662+Arwalu13@users.noreply.github.com>
2022-11-23 20:31:09 -0500

    Stoof

--- j2/CCC10J2.py
+++ j2/CCC10J2.py
@@ -1,8 +1,14 @@
-#Temp Skipped
-a = int(input())
-b = int(input())
-c = int(input())
-d = int(input())
+nForward = int(input())
+nBackward = int(input())
+bForward = int(input())
+bBackward = int(input())
 s = int(input())
 
+nPos = 0
+bPos = 0
+
+NcurrentNum = 0
+BcurrentNum = 0
+
 for i in range(s):
+    if NcurrentNum == nForward:
